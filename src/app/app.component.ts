bbd2b846438565811782c781b341c6c8df00e840
Pranav Jain <pranav19sept@gmail.com>
2020-01-25 16:27:18 +0530

    Updated Back Button Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AcademicDB';
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  onBack() {
+    this.router.navigate(['../'], { relativeTo: this.route })
+  }
+
 }
